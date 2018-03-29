f934b97d03d8d242d4cf0b0f9e199dc30c67fe1a
George Simos <georgesimos@hotmail.gr>
2018-03-29 10:46:16 +0300

    Initial commit

---
+++ src/containers/Reviews/Header/ProductRatingAvg.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../../index.css';
+
+const productRatingAvg = (props) => {
+    return (
+        
+        <div className="zevioo-rating__avg">
+            <div className="zevioo-avg-text">{props.OR.toFixed(1)}<span> από 5 </span> </div>
+            <div className="zevioo-counter__text"><span>{props.RC}</span> αξιολογήσεις</div>
+        </div>
+    )
+}
+export default productRatingAvg;
